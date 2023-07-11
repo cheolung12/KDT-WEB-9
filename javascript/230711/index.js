7bd347bbf680420ec49d686ba3086ab57f6f8096
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-07-11 20:47:17 +0900

    prac: js (function, for, while)

---
+++ javascript/230711/index.js
@@ -0,0 +1,65 @@
+// const multiply = (num1, num2) => {
+//     return num1 * num2;
+// }
+// console.log(multiply(3,7));
+// console.log(multiply(2,2));
+
+
+// const square = (num) => {
+//     return num * num;
+// }
+
+// console.log(square(4));
+// console.log(square(11));
+// console.log(square(5));
+
+// let num1 = prompt("첫번째 숫자를 입력해주세요");
+// let num2 = prompt("두번째 숫자를 입력해주세요");
+
+// const add = (num1, num2) => alert(parseInt(num1) + parseInt(num2));
+// const sub = (num1, num2) => alert(parseInt(num1) - parseInt(num2));
+// const mul = (num1, num2) => alert(parseInt(num1) * parseInt(num2));
+// const divide = (num1, num2) => alert(parseInt(num1) / parseInt(num2));
+
+// let age = Number(prompt("나이를 입력해주세요"));
+// let s = prompt("성별을 입력해주세요");
+
+// if(age >= 20)
+//     alert(`성인 ${s}`);
+// else if(age >= 17)
+//     alert(`고등학생 ${s}`);
+// else if(age >= 14)
+//     alert(`중학생 ${s}`);
+// else if(age >= 8)
+//     alert(`초등학생 ${s}`);
+// else if(age >= 0)
+//     alert(`유아 ${s}`);
+
+// let now = new Date().getHours();
+// console.log(now < 12 ? '오전' : '오후');
+
+// let num = Number(prompt("숫자를 입력해주세요!"));
+// const arr = [];
+// for(let i=0; i <= num; i++){
+//     if(i % 2 !== 0 && i % 13 === 0)
+//         arr.push(i);
+// }
+// console.log(...arr);
+
+// for(let i=1; i<=9; i++){
+//     console.log(`---${i}단---`)
+//     for(let j=1; j<=9; j++){
+//         console.log(`${i} x ${j} = ${i*j}`)
+//     }
+// }
+
+// let sum = 0;
+// let n = 1;
+// while(n < 100){
+//     if (n % 2 == 0 || n % 3 == 0){
+//         sum += n;
+//     }
+//     n++;
+// }
+// console.log(sum);
+

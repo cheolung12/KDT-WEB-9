0d0d193341ef5df6d8a0fe0e80ecfeee42732e09
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-09-21 20:38:24 +0900

    Update App.js

--- react/Syntax/src/App.js
+++ react/Syntax/src/App.js
@@ -1,18 +1,8 @@
-import {
-  ClassComponent,
-  FunctionComponent,
-  Event,
-  EventClass,
-} from '../src/components/0919_component';
 import '../src/App.css';
 
 function App() {
   return (
     <>
-      <ClassComponent age={10} />
-      <FunctionComponent myclass={'kdt9'} />
-      <Event /> <br />
-      <EventClass />
     </>
   );
 }

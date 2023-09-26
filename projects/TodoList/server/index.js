33985d1d1ec078d1f01ae8c7fefea9cf2939daf6
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-09-27 01:41:53 +0900

    server - initial setting

---
+++ projects/TodoList/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const db = require('./models');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// router
+const todoRouter = require('./routes/todo');
+app.use('/todo', todoRouter);
+
+db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+});
+
+

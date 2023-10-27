bc50e38fb83d26184e1c371a8f5638b6529d1f22
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-10-27 16:20:03 +0900

    create spring project

---
+++ spring/kdt-spring/src/main/java/com/kdt/kdtspring/KdtSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.kdtspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdtSpringApplication.class, args);
+	}
+
+}
---
+++ spring/kdt-spring/src/test/java/com/kdt/kdtspring/KdtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.kdtspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
